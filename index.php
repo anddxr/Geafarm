68d8958aa5984842c8b161bc2a043acd92d4216f
anddxr <55501437+anddxr@users.noreply.github.com>
2019-09-28 23:14:39 +0300

    2
    some

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('displey_errors',1);
+ini_set('displey_sturtup_errors',1);
+error_reporting(E-ALL);
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
